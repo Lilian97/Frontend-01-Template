9b777127326a94a26da58d0d48a40c7de36aedb6
Lilian97 <13642147806@163.com>
2020-07-13 23:55:16 +0800

    update:component

--- week14/component/main.js
+++ week14/component/main.js
@@ -1,22 +1,83 @@
 // require("./foo.js");
 
-function create(Cls, attributes){
-    let o = new Cls;
+function createElement(Cls, attributes,...children){
+    let o = new Cls({
+        timer:{}
+    });
 
     for(let name in attributes){
-        o[name] = attributes[name];
+        o.setAttribute(name, attributes[name]);
+    }
+
+    for(let child of children){
+        o.appendChild(child);
     }
 
     return o;
-    // console.log(arguments);
 }
 
 
 class Div {
+    constructor(config){
+        this.children = [];
+        this.root = document.createElement("div");
+    }
 
+    setAttribute(name,value){       //attribute
+        this.root.setAttribute(name, value);
+    }
+
+    appendChild(child){
+        this.children.push(child);
+    }
+
+    mountTo(parent){
+        parent.appendChild(this.root);
+        for(let child of this.children){
+            child.mountTo(this.root);
+        }
+    }
+
+
+    /*
+    set cls(v){   //property
+        console.log("Parent::class", v);
+    }
+    set id(v){
+        console.log("Parent::id", v);
+    }
+    */
+    
 }
 
-let component = <Div id="a" class="b" />
+/*
+class Child {
+    constructor(config){
+        this.children = [];
+        this.root = document.createElement("div");
+    }
+
+    setAttribute(name,value){       //attribute
+        this.root.setAttribute(name, value);
+    }
+
+    appendChild(child){
+        child.mountTo(this.root);
+    }
+
+    mountTo(parent){
+        parent.appendChild(this.root);
+    }
+}
+*/
+
+let component = <Div id="a" cls="b" style="width:100px;height:100px;background:lightgreen;">
+    <Div></Div>
+    <Div></Div>
+    <Div></Div>
+</Div>
+
+component.mountTo(document.body);
 
 console.log(component);
 // component.setAttribute("id", "a");
