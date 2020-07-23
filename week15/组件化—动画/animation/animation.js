09663bbc74201df57498ac511e7139613209ad8d
lilian97 <13642147806@163.com>
2020-07-23 18:19:10 +0800

    update

--- week15/组件化—动画/animation/animation.js
+++ week15/组件化—动画/animation/animation.js
@@ -1,4 +1,7 @@
 export class Timeline {
+    constructor(){
+        
+    }
     tick(){
         console.log("tick");
         requestAnimationFrame(()=>this.tick());
@@ -6,8 +9,21 @@ export class Timeline {
     start(){
         this.tick();
     }
+    add(animation){
+
+    }
 }
 
 export class Animation {
-    
+    constructor(object, property, start, end, duration, delay, timingFunction){
+        this.object = object;
+        this.property = property;
+        this.start = start;
+        this.end = end;
+        this.duration = duration;
+        this.delay = delay;
+        this.timingFunction = timingFunction || ((start, end) => {
+            return (t) => start + t * (end - start)
+        })
+    }
 }
